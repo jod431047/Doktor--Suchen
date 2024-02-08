bed65836dcbbd7b8d17beb4142d0ec1457422a40
jod431047 <jod431047@gmail.com>
2024-02-08 19:13:44 +0100

    add profile in model

--- todo/models.py
+++ todo/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as _
 
 class Profile(models.Model):
+    user = models.OneToOneField(User , verbose_name=("USER"), on_delete=models.CASCADE)
     
     
     class Meta:
