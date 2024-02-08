4cb3a8339ab70abea2a8eafc6eb2162e3ae415d3
jod431047 <jod431047@gmail.com>
2024-02-08 19:06:25 +0100

    add model

--- project/urls.py
+++ project/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('todo/',include('todo.urls')),
+    path('todo/',include('todo.urls',namespace='todo')),
 ]
 
 
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    
+    
+    class Meta:
+        verbose_name = ("Profile")
+        verbose_name_plural = ("Profiles")
+        
+    def __str__(self):
+        return self.name    
+
