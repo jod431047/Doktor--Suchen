a254923d82ed9c435616c359cc413f71b641a5f9
jod431047 <jod431047@gmail.com>
2024-02-09 22:32:26 +0100

    add profile media & and image in model

--- todo/models.py
+++ todo/models.py
@@ -2,8 +2,9 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 
+
 class Profile(models.Model):
-    user = models.OneToOneField(User , verbose_name=("user"), on_delete=models.CASCADE)
+    user = models.ForeignKey(User , verbose_name=("user"), on_delete=models.CASCADE)
     name = models.CharField(_("name:"),max_length=50)
     who_i = models.TextField(_("who I am:"),max_length=250)
     price = models.IntegerField(_("disclosure price:"))
@@ -14,5 +15,6 @@ class Profile(models.Model):
         verbose_name_plural = ("Profiles")
         
     def __str__(self):
-        return self.who_i    
-
+        return '%s' %(self.name)
+    
+    
--- todo/views.py
+++ todo/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+from .models import Profile
 
 
 
 def doctors_list(request):
-    doctors = User.objects.all()
+    doctors = Profile.objects.all()
     return render(request , 'user/doctors_list.html', {
         'doctors' : doctors,
     })
