6aef7b75568f6b9b6dd74e0c3cc338ef6d1e60a1
jod431047 <jod431047@gmail.com>
2024-02-10 22:09:52 +0100

    templates html übersetzen

--- todo/models.py
+++ todo/models.py
@@ -6,9 +6,9 @@ from django.utils.translation import gettext_lazy as _
 class Profile(models.Model):
     user = models.ForeignKey(User , verbose_name=("user"), on_delete=models.CASCADE)
     name = models.CharField(_("name:"),max_length=50)
-    who_i = models.TextField(_("who I am:"),max_length=250)
-    price = models.IntegerField(_("disclosure price:"))
-    image = models.ImageField(_("personal poto"),upload_to='profile')
+    who_i = models.TextField(_("wer ich bin:"),max_length=250)
+    price = models.IntegerField(_("untersuchengs price:"))
+    image = models.ImageField(_("personal foto"),upload_to='profile')
                                   
     class Meta:
         verbose_name = ("Profile")
